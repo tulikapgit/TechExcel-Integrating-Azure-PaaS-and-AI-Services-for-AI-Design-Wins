f6cd4e43331986cf5067214b4fb9f192a9c48b2d
tulikapgit <90724728+tulikapgit@users.noreply.github.com>
2025-03-18 14:26:57 +0000

    updated code

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-seciohu5rqhky FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-seciohu5rqhky;
+ALTER ROLE db_datawriter ADD MEMBER id-seciohu5rqhky;
 
